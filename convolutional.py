13de4fb4c9f69a926194f599133ae046d67b36eb
agentle <latiosa811@charter.net>
2018-12-05 17:20:34 -0500

    updated CNN

--- convolutional.py
+++ convolutional.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import os
-
+from PIL import Image
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 '''
 Potential things to change:
@@ -13,27 +14,34 @@ Potential things to change:
 - node units in dense layer
 '''
 
-def cnn_model_functions(features, labels, mode, lr=0.001):
+IMG_RES = 28
+
+
+def cnn_model_functions(features, labels, mode):
+
+	print("Starting NN")
 
 	# create input layer
-	input_layer = tf.reshape(features["x"], [-1, 64, 64, 3])
+	input_layer = tf.reshape(features["x"], [-1, 28, 28, 3])
+
+	print("Done with input layer")
 
 	# TODO: check filter number; currently maintaining 1:1 then 2:1
 	con_layer1 = tf.layers.conv2d(
 		inputs = input_layer,
-		filters = 64,
+		filters = 32,
 		kernel_size = [5, 5],
 		padding="same",
 		activation = tf.nn.relu)
 
 	pool_layer1 = tf.layers.max_pooling2d(
 		inputs=con_layer1,
-		pool_size=[4,4], 
-		strides=4)
+		pool_size=[2,2], 
+		strides=2)
 
 	con_layer2 = tf.layers.conv2d(
 		inputs = pool_layer1,
-		filters = 128,
+		filters = 64,
 		kernel_size = [5,5],
 		padding = "same",
 		activation = tf.nn.relu)
@@ -47,7 +55,7 @@ def cnn_model_functions(features, labels, mode, lr=0.001):
 	# TODO: there are 3 channels in input due to color image; 
 	# is that relationship maintained in future layer channels?
 	pool_flat = tf.reshape(pool_layer2,
-		[-1, 8 * 8 * 128 * 3])
+		[-1, 7 * 7 * 64 * 3])
 
 	# create fully connected layer
 	dense = tf.layers.dense(
@@ -84,7 +92,7 @@ def cnn_model_functions(features, labels, mode, lr=0.001):
 
 	# training
 	if mode == tf.estimator.ModeKeys.TRAIN:
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
 		train_op = optimizer.minimize(
 			loss=loss,
 			global_step=tf.train.get_global_step())
@@ -102,6 +110,8 @@ def cnn_model_functions(features, labels, mode, lr=0.001):
 			loss=loss, 
 			eval_metric_ops=eval_metric_ops)
 
+# def get_data(train_folder, eval_folder):
+# 	return (train_data, eval_data)
 
 def train(train_data, train_labels, classifier, iterations=50):
 	
@@ -141,16 +151,16 @@ def main():
 	eval_labels = []
 
 	# preprocess images to 64 x 64 numpy arrays
-	bulk = get_data("training_set", "test_set")
+	bulk = get_data("train_set_small", "test_set_small")
 
 	# grab data
 	for key, value in bulk[0].items():
-		train_data.add(key)
-		train_labels.add(value)
+		train_data.append(key)
+		train_labels.append(value)
 
 	for key, value in bulk[1].items():
-		eval_data.add(key)
-		eval_labels.add(value)
+		eval_data.append(key)
+		eval_labels.append(value)
 
 	# cast to numpy arrays
 	train_data = np.asarray(train_data)
@@ -198,6 +208,10 @@ def get_folder_elements(folder):
 
 	for _file in file_names:
 		print(_file)
+		size = IMG_RES, IMG_RES
+		im = Image.open(folder + "/" + _file)
+		im_resized = im.resize(size, Image.ANTIALIAS)
+		im_resized.save(folder + "/" + _file, "PNG")
 		img = load_img(folder + "/" + _file)  # this is a PIL image
 		# Convert to Numpy Array
 		x = img_to_array(img)  
@@ -209,10 +223,10 @@ def get_folder_elements(folder):
 
 
 def convert_image_to_64x64(array, height, width):
-	num_per_bucket = np.zeros((64, 64, 3))
-	new_array = np.zeros((64, 64, 3))
-	height_ratio = height/64
-	width_ratio = width/64
+	num_per_bucket = np.zeros((IMG_RES, IMG_RES, 3))
+	new_array = np.zeros((IMG_RES, IMG_RES, 3))
+	height_ratio = height/IMG_RES
+	width_ratio = width/IMG_RES
 
 
 	for row in range(0, height):
@@ -221,8 +235,8 @@ def convert_image_to_64x64(array, height, width):
 				new_array[int(row//height_ratio)][int(col//width_ratio)][rgb] += array[row][col][rgb]
 				num_per_bucket[int(row//height_ratio)][int(col//width_ratio)][rgb] += 1
 
-	for row in range(0, 64):
-		for col in range(0, 64):
+	for row in range(0, IMG_RES):
+		for col in range(0, IMG_RES):
 			for rgb in range(0, 3):
 				new_array[row][col][rgb] = int(new_array[row][col][rgb]/num_per_bucket[row][col][rgb])
 
